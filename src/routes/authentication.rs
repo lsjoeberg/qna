da85de65c19220b1a85a0c98e86b198f31f1a8c9
Lars Sjöberg <lsjoeberg@protonmail.com>
2024-01-23 22:53:45 +0100

    Store hashed salted passwords in accounts

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,11 +1,26 @@
+use argon2::Config;
+use rand::random;
 use reqwest::StatusCode;
 
 use crate::account::Account;
 use crate::store::Store;
 
 pub async fn register(store: Store, account: Account) -> Result<impl warp::Reply, warp::Rejection> {
+    let hashed_password = hash_password(account.password.as_bytes());
+    let account = Account {
+        id: account.id,
+        email: account.email,
+        password: hashed_password,
+    };
+
     match store.add_account(account).await {
         Ok(_) => Ok(warp::reply::with_status("Account created", StatusCode::OK)),
         Err(err) => Err(warp::reject::custom(err)),
     }
+}
+
+pub fn hash_password(password: &[u8]) -> String {
+    let salt = random::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config).unwrap()
 }
