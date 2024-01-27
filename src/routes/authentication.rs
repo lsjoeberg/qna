b58081efe06fcf4d82d16c575ee2ccfec50c1012
Lars Sjöberg <lsjoeberg@protonmail.com>
2024-01-27 17:28:14 +0100

    Remove OpenSSL dependencies
    Swap the 'paseto' lib for 'pasetors'.

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -2,6 +2,13 @@ use std::future;
 
 use argon2::Config;
 use chrono::Utc;
+use pasetors::{
+    claims::{Claims, ClaimsValidationRules},
+    keys::SymmetricKey,
+    token::UntrustedToken,
+    version4::V4,
+    Local,
+};
 use rand::random;
 use reqwest::StatusCode;
 use warp::Filter;
@@ -59,25 +66,33 @@ fn issue_token(account_id: AccountId) -> String {
     let dt = current_date_time + chrono::Duration::days(1);
     let key = std::env::var("PASETO_KEY").expect("Env var PASETO_KEY must be set");
 
-    paseto::tokens::PasetoBuilder::new()
-        .set_encryption_key(key.as_bytes())
-        .set_expiration(&dt)
-        .set_not_before(&Utc::now())
-        .set_claim("account_id", serde_json::json!(account_id))
-        .build()
+    let key = SymmetricKey::<V4>::from(key.as_bytes()).expect("Invalid PASETO_KEY");
+    let mut claims = Claims::new().unwrap();
+    claims
+        .add_additional("account_id", serde_json::json!(account_id))
+        .expect("Failed to set claim 'account_id'");
+    claims
+        .expiration(&dt.to_rfc3339())
+        .expect("Failed to set claim 'exp'");
+    claims
+        .not_before(&Utc::now().to_rfc3339())
+        .expect("Failed to set claim 'nbf'");
+    pasetors::local::encrypt(&key, &claims, None, None)
         .expect("Failed to construct paseto token with builder!")
 }
 
 pub fn verify_token(token: String) -> Result<Session, handle_errors::Error> {
     let key = std::env::var("PASETO_KEY").expect("Env var PASETO_KEY must be set");
-    let token = paseto::tokens::validate_local_token(
-        &token,
-        None,
-        key.as_bytes(),
-        &paseto::tokens::TimeBackend::Chrono,
-    )
-    .map_err(|_| handle_errors::Error::CannotDecryptToken)?;
-    serde_json::from_value::<Session>(token).map_err(|_| handle_errors::Error::CannotDecryptToken)
+    let key = SymmetricKey::<V4>::from(key.as_bytes()).expect("Invalid PASETO_KEY");
+    let validation_rules = ClaimsValidationRules::new();
+    let untrusted_token = UntrustedToken::<Local, V4>::try_from(&token)
+        .map_err(|_| handle_errors::Error::CannotDecryptToken)?;
+    let trusted_token =
+        pasetors::local::decrypt(&key, &untrusted_token, &validation_rules, None, None)
+            .map_err(|_| handle_errors::Error::CannotDecryptToken)?;
+
+    serde_json::from_str::<Session>(trusted_token.payload())
+        .map_err(|_| handle_errors::Error::CannotDecryptToken)
 }
 
 pub fn auth() -> impl Filter<Extract = (Session,), Error = warp::Rejection> + Clone {
