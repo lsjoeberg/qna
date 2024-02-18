d4192f005eee9eab3dd82351f46b0425c3870d1f
Lars Sjöberg <lsjoeberg@protonmail.com>
2024-02-18 17:51:37 +0100

    Add GET /questions route with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-use warp::Filter;
+use serde::Serialize;
+use std::str::FromStr;
+use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
 
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(QuestionId(s.to_string()))
+    }
+}
+
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
     content: String,
     tags: Option<Vec<String>>,
 }
-
-struct QuestionId(String);
 
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -20,12 +36,43 @@ impl Question {
     }
 }
 
+async fn get_questions() -> Result<impl Reply, Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No ID provided."),
+        String::from("title"),
+        String::from("content"),
+        Some(vec![String::from("faq")]),
+    );
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(_invalid_id) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let hello = warp::get()
-        .map(|| "Hello, World!");
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 7878))
-        .await;
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 7878)).await;
 }
